0bbf108be3a6fe627fa330308a901a08345ffd7e
Yan Song <yansong@149-160-160-247.dhcp-bl.indiana.edu>
2016-09-15 13:09:46 -0400

    Checked version for final submission

--- shell/xsh_hello.c
+++ shell/xsh_hello.c
@@ -1,12 +1,14 @@
 #include <xinu.h>
 #include <stdio.h>
 
-shellcmd xsh_hello (int nargs, char *args[]) {
-  if (nargs < 2) {
-    printf("error: too few arguments\n");}
+shellcmd xsh_hello (int32 nargs, char *args[]) {
+  if (nargs < 2) {    
+	printf("error: too few arguments\n");
+	return 1;}
   else if (nargs >2) {
-    printf("error: too many arguments\n");}
+	printf("error: too many arguments\n");
+	return 2;}
   else {
-    printf("Hello %s. Welcome to the world of Xinu!!\n", args[1]);}
-  return 0;
+	printf("Hello %s. Welcome to the world of Xinu!!\n", args[1]);
+	return 0;}
 }	
