0ee3250cf6c6b80acaee6e63f7ac453d8dd9553b
Yan Song <yansong@149-160-161-179.dhcp-bl.indiana.edu>
2016-09-22 13:58:47 -0400

    Finished version of assigment 3

--- include/prodcons.h
+++ include/prodcons.h
@@ -2,5 +2,8 @@
 extern int32 n; /*this is just declaration*/
 
 /*function Prototype*/
-void consumer(int32 count);
-void producer(int32 count);
+void consumer(int32 count, sid32 consumed, sid32 produced);
+void producer(int32 count, sid32 consumed, sid32 produced);
+
+/* Semaphore Declarations */
+extern sid32 produced, consumed; 
